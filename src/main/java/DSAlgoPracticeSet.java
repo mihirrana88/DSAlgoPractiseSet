ac44dffb4df9b9ac73724d72f182d0afd58ff701
mihirrajsinh.rana <mihirrana88@gmail.com>
2022-01-29 22:31:45 +0530

    use personal email to commit in personal github.com

--- src/main/java/DSAlgoPracticeSet.java
+++ src/main/java/DSAlgoPracticeSet.java
@@ -22,6 +22,6 @@ public class DSAlgoPracticeSet {
         //Problem.leftViewOfBinaryTree(scanner);
         //Problem.rightViewOfBinaryTree(scanner);
         //Problem.topViewOfBinaryTree(scanner);
-        //Problem.bottomViewOfBinaryTree(scanner);
+        Problem.bottomViewOfBinaryTree(scanner);
     }
 }
