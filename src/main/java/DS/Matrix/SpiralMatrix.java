f11eeb06c8f0150da57b8e7869b068b6cc1e62b1
mihirrajsinh.rana <mihirrana88@gmail.com>
2022-05-17 22:00:41 +0530

    Added ==> Matrix >> SpiralMatrix

---
+++ src/main/java/DS/Matrix/SpiralMatrix.java
@@ -0,0 +1,74 @@
+package DS.Matrix;
+
+import Exception.InvalidInputArgument;
+
+public class SpiralMatrix {
+    int[][] matrix = null;
+
+    public void getInputLength(int row, int column){
+        matrix = new int[row][column];
+    }
+
+    public void acceptInput(int num, int row, int column){
+        if(row < 0 || column < 0 || row > matrix.length-1 || column > matrix[0].length){
+            throw new ArrayIndexOutOfBoundsException("row="+row+" and column="+column);
+        }
+        matrix[row][column] = num;
+    }
+
+    public int[] spiralOrder() throws Exception{
+        if(null == matrix || matrix.length == 0){
+            throw new InvalidInputArgument(matrix);
+        }
+
+        int[] spiral = new int[matrix.length];
+        int cursor = 0;
+
+        int rowBegin=0;
+        int rowEnd=matrix.length-1;
+        int colBegin=0;
+        int colEnd=matrix[0].length-1;
+
+        if(matrix.length==0)
+            return spiral;
+
+        while (true)
+        {
+            //left to right
+            for(int i=colBegin;i<=colEnd;i++)
+            {
+                spiral[cursor] = matrix[rowBegin][i];
+                cursor++;
+            }
+            rowBegin++;
+            if(rowBegin > rowEnd ) break;
+
+            //top to buttom
+            for(int i=rowBegin;i<=rowEnd;i++)
+            {
+                spiral[cursor] = matrix[i][colEnd];
+                cursor++;
+            }
+            colEnd--;
+            if(colBegin >  colEnd) break;
+
+            // right to left
+            for(int i=colEnd;i>=colBegin;i--)
+            {
+                spiral[cursor] = matrix[rowEnd][i];
+            }
+            rowEnd--;
+            if(rowBegin > rowEnd ) break;
+
+            //buttom to top
+            for(int i=rowEnd;i>=rowBegin;i--)
+            {
+                spiral[cursor] = matrix[i][colBegin];
+            }
+            colBegin++;
+            if(colBegin >  colEnd) break;
+        }
+
+        return spiral;
+    }
+}
