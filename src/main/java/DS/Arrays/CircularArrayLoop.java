4d6835fca93a97a0c95eed00867eb41446938591
mihirrajsinh.rana <mihirrana88@gmail.com>
2022-05-28 21:55:25 +0530

    Added ==> Arrays >> CircularArrayLoop

---
+++ src/main/java/DS/Arrays/CircularArrayLoop.java
@@ -0,0 +1,49 @@
+package DS.Arrays;
+
+public class CircularArrayLoop {
+    public int[] nums = null;
+
+    public void getInputLength(int len){
+        nums = new int[len];
+    }
+
+    public void acceptInput(int num, int index){
+        if(index < 0 || index >= nums.length){
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
+        nums[index] = num;
+    }
+
+    public boolean circularArrayLoop(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            float direction = Math.signum(nums[i]);
+            int slow = i;
+            int fast = i;
+
+            do {
+                slow = getNextIndex(nums, direction, slow);
+                fast = getNextIndex(nums, direction, fast);
+                if (fast != -1)
+                    fast = getNextIndex(nums, direction, fast);
+                if (fast == -1 || nums[slow] == 0 || nums[fast] == 0)
+                    break;
+            } while (slow != fast);
+
+            if (slow != -1 && slow == fast)
+                return true;
+            nums[i] = 0;
+        }
+        return false;
+    }
+
+    private int getNextIndex(int[] nums, float direction, int i) {
+        float currentDirection = Math.signum(nums[i]);
+        if (currentDirection * direction < 0)
+            return -1;
+        int n = nums.length;
+        int nextIndex = (i + nums[i]) % n;
+        if (nextIndex < 0)
+            nextIndex += n;
+        return nextIndex == i ? -1 : nextIndex;
+    }
+}
