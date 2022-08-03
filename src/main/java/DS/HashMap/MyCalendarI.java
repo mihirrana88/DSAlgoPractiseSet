501b1d7fe33379e852b59eda905695fe8354b7a5
mihirrajsinh.rana <mihirrana88@gmail.com>
2022-08-03 21:09:46 +0530

    Added ==> HashMap >> MyCalendarI

--- src/main/java/DS/HashMap/MyCalendarI.java
+++ src/main/java/DS/HashMap/MyCalendarI.java
@@ -36,7 +36,7 @@ myCalendar.book(10, 20); // return True
 myCalendar.book(15, 25); // return False, It can not be booked because time 15 is already booked by another event.
 myCalendar.book(20, 30); // return True, The event can be booked, as the first event takes every time less than 20,
 but not including 20.
- 
+
 Constraints:
 
 0 <= start < end <= 109
