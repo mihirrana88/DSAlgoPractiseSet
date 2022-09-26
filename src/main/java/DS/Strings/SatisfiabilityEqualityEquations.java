7697d2440fa9773928ac84fafa3db6ea9f7d5ce5
mihirrajsinh.rana <mihirrana88@gmail.com>
2022-09-26 22:47:07 +0530

    Added ==> Strings >> SatisfiabilityEqualityEquations

---
+++ src/main/java/DS/Strings/SatisfiabilityEqualityEquations.java
@@ -0,0 +1,88 @@
+package DS.Strings;
+
+import Exception.InvalidInputArgument;
+
+/*
+Satisfiability of Equality Equations
+
+You are given an array of strings equations that represent relationships between variables where each string
+equations[i] is of length 4 and takes one of two different forms: "xi==yi" or "xi!=yi".Here, xi and yi
+are lowercase letters (not necessarily different) that represent one-letter variable names.
+
+Return true if it is possible to assign integers to variable names so as to satisfy all the given equations,
+or false otherwise.
+
+Example 1:
+Input: equations = ["a==b","b!=a"]
+Output: false
+Explanation: If we assign say, a = 1 and b = 1, then the first equation is satisfied, but not the second.
+There is no way to assign the variables to satisfy both equations.
+
+Example 2:
+Input: equations = ["b==a","a==b"]
+Output: true
+Explanation: We could assign a = 1 and b = 1 to satisfy both equations.
+
+Constraints:
+
+1 <= equations.length <= 500
+equations[i].length == 4
+equations[i][0] is a lowercase letter.
+equations[i][1] is either '=' or '!'.
+equations[i][2] is '='.
+equations[i][3] is a lowercase letter.
+
+*/
+
+public class SatisfiabilityEqualityEquations {
+    public String[] inputString = null;
+    static int[] par;
+
+    public void getInputLength(int len){
+        inputString = new String[len];
+    }
+
+    public void acceptInput(String str, int index){
+        if(index < 0 || index >= inputString.length){
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
+        inputString[index] = str;
+    }
+
+    public static int findLeaderParent(int u){
+        return par[u]==u ? u : (par[u]=findLeaderParent(par[u]));
+    }
+
+    public boolean equationsPossible(String[] equations) {
+
+        par=new int[26];
+        for(int i=0;i<26;i++){
+            par[i]=i;
+        }
+
+        int[] notArray=new int[26];
+
+        for(String str : equations){
+            if(str.charAt(1) == '='){
+                int p1=findLeaderParent(str.charAt(0) - 'a');
+                int p2=findLeaderParent(str.charAt(3) - 'a');
+
+                if(p1!=p2){
+                    par[p2]=p1;
+                }
+            }
+        }
+
+        for(String str : equations){
+            if(str.charAt(1)=='!'){
+                int p1=findLeaderParent(str.charAt(0) - 'a');
+                int p2=findLeaderParent(str.charAt(3) - 'a');
+
+                if(p1==p2){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
