8ca759853f496252a1c83493084ba9d747047625
mihirrajsinh.rana <mihirrana88@gmail.com>
2022-10-04 22:34:24 +0530

    Added ==> BinaryTree >> PathSum

--- src/main/java/DS/Tree/BinaryTree/PathSum.java
+++ src/main/java/DS/Tree/BinaryTree/PathSum.java
@@ -51,7 +51,7 @@ public class PathSum {
         }
         this.binaryTreeInput = binaryTreeInput;
     }
-    
+
     public boolean hasPathSum(){
         return hasPathSum(binaryTreeInput.root, targetSum);
     }
