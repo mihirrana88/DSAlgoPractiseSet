82fa78da3199762b0ce3353f0bfd2649b62f79a8
mihirrajsinh.rana <mihirrana88@gmail.com>
2022-10-20 21:58:50 +0530

    Added ==> HashMap >> IntegerToRoman

--- src/main/java/DS/HashMap/IntegerToRoman.java
+++ src/main/java/DS/HashMap/IntegerToRoman.java
@@ -1,4 +1,87 @@
 package DS.HashMap;
 
+import java.util.TreeMap;
+
+/*
+Integer to Roman
+
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII,
+which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four
+is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it
+making four. The same principle applies to the number nine, which is written as IX. There are six
+instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9.
+X can be placed before L (50) and C (100) to make 40 and 90.
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given an integer, convert it to a roman numeral.
+
+Example 1:
+Input: num = 3
+Output: "III"
+Explanation: 3 is represented as 3 ones.
+
+Example 2:
+Input: num = 58
+Output: "LVIII"
+Explanation: L = 50, V = 5, III = 3.
+
+Example 3:
+Input: num = 1994
+Output: "MCMXCIV"
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+Constraints:
+
+1 <= num <= 3999
+
+*/
+
 public class IntegerToRoman {
+    int num;
+
+    public void getNum(int num){
+        this.num = num;
+    }
+
+    public String intToRoman() {
+        TreeMap<Integer, String> map = new TreeMap<>();
+        map.put(1, "I");
+        map.put(5, "V");
+        map.put(10, "X");
+        map.put(50, "L");
+        map.put(100, "C");
+        map.put(500, "D");
+        map.put(1000, "M");
+        map.put(4, "IV");
+        map.put(9, "IX");
+        map.put(40, "XL");
+        map.put(90, "XC");
+        map.put(400, "CD");
+        map.put(900, "CM");
+
+        String ans = "";
+
+        while(num > 0){
+            int floorKey = map.floorKey(num);
+            ans = ans + map.get(floorKey);
+            num = num - floorKey;
+        }
+
+        return ans;
+    }
+
 }
