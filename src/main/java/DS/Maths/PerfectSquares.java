fbd64c8f6e27fd7afa030e61d4f8c2cd4f5a4e51
mihirrajsinh.rana <mihirrana88@gmail.com>
2022-11-22 22:56:21 +0530

    Added ==> Maths >> PerfectSquares

--- src/main/java/DS/Maths/PerfectSquares.java
+++ src/main/java/DS/Maths/PerfectSquares.java
@@ -2,7 +2,9 @@ package DS.Maths;
 
 import Exception.InvalidInputArgument;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /*
 Perfect Squares
@@ -64,5 +66,38 @@ public class PerfectSquares {
             if(base * base == (n - i * i)) return 2;
         }
         return 3;
+    }
+
+
+    /*
+    * Intuition
+
+        * First we need to build the squares array that represent the possible square values that we
+          can have upto the given number n.
+        * Then it becomes a coin-change problem. We need to pick the least amount of values that can
+          form our number n.
+ 
+    * */
+    public int numSquaresv3() {
+        List<Integer> squares = new ArrayList<>();
+
+        int cur = 1;
+        // Build the squares array
+        while (Math.pow(cur, 2) <= n) {
+            squares.add((int) Math.pow(cur++, 2));
+        }
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        // Base case
+        dp[0] = 0;
+        for (int i = 1; i < n+1; i++ ) {
+            // DP transition
+            for (int j = 0; j < squares.size() && squares.get(j) <= i; j++) {
+                dp[i] = Math.min(dp[i], 1 + dp[i-squares.get(j)]);
+            }
+        }
+        return dp[n];
     }
 }
